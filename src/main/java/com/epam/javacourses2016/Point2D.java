f1c7f76d59b65af170421dbb267751d62b63603a
Nikolai Karulin <nikolai.karulin@yandex.ru>
2016-11-13 22:37:09 +0300

    15  skeleton + Point2D

---
+++ src/main/java/com/epam/javacourses2016/Point2D.java
@@ -0,0 +1,20 @@
+package com.epam.javacourses2016;
+
+public class Point2D {
+
+    private double x;
+    private double y;
+
+    public Point2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
---
+++ src/main/java/com/epam/javacourses2016/task15/SolverTask15.java
@@ -0,0 +1,49 @@
+package com.epam.javacourses2016.task15;
+
+import com.epam.javacourses2016.Point2D;
+
+import java.io.File;
+import java.util.Set;
+
+/**
+ * На плоскости задано N точек.
+ * Вывести в файл описания всех прямых, которые проходят более чем через 2 точки из заданных.
+ */
+public class SolverTask15 {
+    /**
+     * Осуществляет анализ переданных точек, вычисляя линии, которые проходят более чем через 2 точки.
+     * @param points Множество точек на плоскости.
+     * @param output Файл для вывода результатов.
+     * @return Файл с результатами анализа.
+     */
+    IFileWithLines analyze(Set<Point2D> points, File output) {
+        //TODO
+        return null;
+    }
+
+    /**
+     * Представляет файл, содержащий информацию о найденных линиях.
+     */
+    interface IFileWithLines {
+
+        /**
+         * @return Файл с результатами анализа.
+         */
+        File getFile();
+
+        /**
+         * Извлекает из файла информацию о хранящихся в нем линиях.
+         * @return Множество линий, найденных в результате анализа.
+         */
+        Set<ILine> getLines();
+    }
+
+    /**
+     * Прямая, заданная точками, входящими в исходное множество.
+     */
+    interface ILine {
+
+        /** @return Точки, через которые проходит прямая */
+        Set<Point2D> getPoints();
+    }
+}
