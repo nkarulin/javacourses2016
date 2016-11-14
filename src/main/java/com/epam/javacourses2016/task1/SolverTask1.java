162ed3a2e760fc59ff768aad71bfbef55d057299
IbrayevRamil <ramil1dan@gmail.com>
2016-11-14 18:19:15 +0300

    task1 done

--- src/main/java/com/epam/javacourses2016/task1/SolverTask1.java
+++ src/main/java/com/epam/javacourses2016/task1/SolverTask1.java
@@ -1,7 +1,11 @@
 package com.epam.javacourses2016.task1;
 
+import com.epam.javacourses2016.task3.Poem;
+
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Ввести строки из файла, записать в список.
@@ -15,8 +19,22 @@ public class SolverTask1 {
      * @param output Файл с выходными данными.
      * @return Список строк, прочитанных из входного файла в прямом порядке.
      */
-    public List<String> reverseFile(File input, File output) {
-        //TODO: realize solver of a task
-        return null;
+    public List<String> sortPoems(Set<Poem> poems, String author) {
+        List<String> lines = new ArrayList<>();
+        for (Poem p : poems) {
+            if (p.getAuthor().equals(author)) {
+                lines = p.getLines();
+            }
+        }
+        for (int i = lines.size() - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (lines.get(j).length() > lines.get(j + 1).length()) {
+                    String t = lines.get(j);
+                    lines.set(j, lines.get(j+1));
+                    lines.set(j+1, t);
+                }
+            }
+        }
+        return lines;
     }
 }
