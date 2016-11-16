1816459c8765aee45448d87eda6969d370cbfdb6
PutienkoEN <justmysimplemail@gmail.com>
2016-11-16 14:40:04 +0300

    Task6 with Test6

--- src/main/java/com/epam/javacourses2016/task6/SolverTask6.java
+++ src/main/java/com/epam/javacourses2016/task6/SolverTask6.java
@@ -1,6 +1,7 @@
 package com.epam.javacourses2016.task6;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Сложить два многочлена, если коэффициенты многочленов хранятся в объекте HashMap.
@@ -11,11 +12,33 @@ public class SolverTask6 {
 
     /**
      * Осуществляет сложение двух многочленов.
-     * @param first Первый многочлен.
+     *
+     * @param first  Первый многочлен.
      * @param second Второй многочлен.
      * @return Многочлен, полученный в результате сложения.
      */
     public HashMap<Integer, Integer> addPolynomials(HashMap<Integer, Integer> first, HashMap<Integer, Integer> second) {
-        return null;
+        HashMap<Integer, Integer> sum = new HashMap<>();
+
+        for (Map.Entry<Integer, Integer> entry : first.entrySet()) {
+            Integer key = entry.getKey();
+            Integer secondValue = 0;
+
+            if (second.containsKey(key)) {
+                secondValue = second.get(key);
+            }
+
+            sum.put(key, entry.getValue() + secondValue);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : second.entrySet()) {
+            Integer key = entry.getKey();
+
+            if (!first.containsKey(key)) {
+                sum.put(key, entry.getValue());
+            }
+        }
+
+        return sum;
     }
 }
