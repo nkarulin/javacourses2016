f3ea9fe9ba2c94c9189696c04c5c41828e7400ee
IbrayevRamil <ramil1dan@gmail.com>
2016-11-16 12:16:41 +0300

    task3 done

--- src/main/java/com/epam/javacourses2016/task3/SolverTask3.java
+++ src/main/java/com/epam/javacourses2016/task3/SolverTask3.java
@@ -1,5 +1,6 @@
 package com.epam.javacourses2016.task3;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -15,8 +16,23 @@ public class SolverTask3 {
      * @param author Автор, стихотворения которого необходимо выбрать.
      * @return Список, упорядоченных по длине строк, составляющих стихотворения автора.
      */
+
     public List<String> sortPoems(Set<Poem> poems, String author) {
-        //TODO: realize solver of a task
-        return null;
+        List<String> lines = new ArrayList<>();
+        for (Poem p : poems) {
+            if (p.getAuthor().equals(author)) {
+                lines = p.getLines();
+            }
+        }
+        for (int i = lines.size() - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (lines.get(j).length() > lines.get(j + 1).length()) {
+                    String t = lines.get(j);
+                    lines.set(j, lines.get(j+1));
+                    lines.set(j+1, t);
+                }
+            }
+        }
+        return lines;
     }
 }
