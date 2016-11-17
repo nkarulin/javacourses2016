8e7818f9433916610cc5c5d5607c761b9c65d068
Yuhans <yushin.tema@gmail.com>
2016-11-17 14:01:54 +0300

    solve task 3

--- src/main/java/com/epam/javacourses2016/task3/SolverTask3.java
+++ src/main/java/com/epam/javacourses2016/task3/SolverTask3.java
@@ -1,7 +1,9 @@
 package com.epam.javacourses2016.task3;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Занести строки, составляющие стихотворения указанного автора, в список.
@@ -17,6 +19,21 @@ public class SolverTask3 {
      */
     public List<String> sortPoems(Set<Poem> poems, String author) {
         //TODO: realize solver of a task
-        return null;
+        List<Poem> poemsOfAuthor = poems.stream()
+                .filter(poem -> poem.getAuthor().equalsIgnoreCase(author))
+                .collect(Collectors.toList());
+        List<String> allLines = new ArrayList<>();
+        for (Poem poem : poemsOfAuthor) {
+            List<String> poemsLines = poem.getLines();
+            allLines.addAll(poemsLines);
+        }
+        allLines.stream().sorted((string1, string2) -> {
+            if (string1.length() > string2.length()) {
+                return 1;
+            } else if (string1.length() < string2.length()) {
+                return -1;
+            } else return 0;
+        }).close();
+        return allLines;
     }
 }
