3364fb285dca0978c85e1efb45fd5373d9da62aa
PutienkoEN <justmysimplemail@gmail.com>
2016-11-18 12:58:12 +0300

    Task 2, 19 test 19

--- src/main/java/com/epam/javacourses2016/task2/SolverTask2.java
+++ src/main/java/com/epam/javacourses2016/task2/SolverTask2.java
@@ -1,6 +1,9 @@
 package com.epam.javacourses2016.task2;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -10,11 +13,20 @@ public class SolverTask2 {
 
     /**
      * Формирует множество всех элементов, входящих в текущий каталог и во все вложенные.
+     *
      * @param directory Корневой каталог.
      * @return Множество элементов корневого каталога и подкаталогов.
      */
-    public Set<File> getFiles(File directory) {
-        //TODO: realize solver of a task
-        return null;
+    public static Set<File> getFiles(File directory) {
+
+        Set<File> files = new HashSet<>();
+        files.add(directory);
+
+        return files;
+    }
+
+    public static void main(String[] args) {
+        File file = new File("src/main/java/com/epam/javacourses2016");
+        Set<File> result = getFiles(file);
     }
 }
--- src/test/java/com/epam/javacourses2016/task19/SolverTask19Test.java
+++ src/test/java/com/epam/javacourses2016/task19/SolverTask19Test.java
@@ -1,6 +1,11 @@
 package com.epam.javacourses2016.task19;
 
+import com.epam.javacourses2016.Car;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.*;
 
 import static org.testng.Assert.*;
 
@@ -8,9 +13,44 @@ import static org.testng.Assert.*;
  * Created by kodoo on 13.11.16.
  */
 public class SolverTask19Test {
-    @Test
-    public void testGetNumberOvertaking() throws Exception {
-        //TODO
+
+    @Test(dataProvider = "cars")
+    public void testGetNumberOvertaking(long lengthLap, long numberLaps, int overtake, Set<Car> cars) throws Exception {
+        SolverTask19 solver = new SolverTask19();
+        Assert.assertEquals(solver.getNumberOvertaking(cars,lengthLap,numberLaps), overtake);
     }
 
+    @DataProvider(name = "cars")
+    private Object[][] cars() {
+        return new Object[][]{
+                {100, 1, 2, new HashSet<Car>() {
+                    {
+                        add(new Car(1, 50));
+                        add(new Car(2, 25));
+                        add(new Car(3, 25));
+                    }
+                }},
+                {100, 5, 6, new HashSet<Car>() {
+                    {
+                        add(new Car(1, 50));
+                        add(new Car(2, 25));
+                        add(new Car(3, 25));
+                    }
+                }},
+                {100, 1, 0,new HashSet<Car>() {
+                    {
+                        add(new Car(1, 25));
+                        add(new Car(2, 25));
+                        add(new Car(3, 50));
+                    }
+                }},
+                {100, 5, 4,new HashSet<Car>() {
+                    {
+                        add(new Car(1, 25));
+                        add(new Car(2, 25));
+                        add(new Car(3, 50));
+                    }
+                }},
+        };
+    }
 }
