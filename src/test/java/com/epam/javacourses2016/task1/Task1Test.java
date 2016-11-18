56e3cc849c6f786f32fb50584c6d63ba6d8c5a76
PutienkoEN <justmysimplemail@gmail.com>
2016-11-18 23:00:51 +0300

    Gitignore (not shure it should be here, but i don't know how remove it)
    Task1Test Edit(added \n)
    Task3Test Edit(added foreach + file route + changed data-source + added white space to poem)
    Task4Test Added
    Task6Test Added
    Task7Test Added
    Task19Test Added
    Car class constructor changed from package-privte to public (for test)

--- src/test/java/com/epam/javacourses2016/task1/Task1Test.java
+++ src/test/java/com/epam/javacourses2016/task1/Task1Test.java
@@ -16,7 +16,7 @@ import java.util.Scanner;
 
 public class Task1Test {
 
-    @Test(enabled = false, dataProvider = "lines")
+    @Test(enabled = true, dataProvider = "lines")
     public void testTask1(String[] linesArray) throws IOException {
         List<String> lines = Arrays.asList(linesArray);
         SolverTask1 solver = new SolverTask1();
@@ -25,7 +25,7 @@ public class Task1Test {
 
         try (FileWriter write = new FileWriter(in)) {
             for (String line : lines) {
-                write.write(line);
+                write.write(line + '\n');
             }
         }
 
--- src/test/java/com/epam/javacourses2016/task7/SolverTask7Test.java
+++ src/test/java/com/epam/javacourses2016/task7/SolverTask7Test.java
@@ -1,13 +1,63 @@
 package com.epam.javacourses2016.task7;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import static org.testng.Assert.*;
 
 public class SolverTask7Test {
-    @Test
-    public void testMultiplyPolynomials() throws Exception {
-        //TODO
+    @Test(dataProvider = "polynomials2")
+    public void testMultiplyPolynomials(List<Integer> first, List<Integer> second,
+                                        List<Integer> result) throws Exception {
+
+        SolverTask7 solver = new SolverTask7();
+        Assert.assertEquals(solver.multiplyPolynomials(first, second), result);
     }
 
+    @DataProvider(name = "polynomials2")
+    public Object[][] polynomials() {
+        return new Object[][]{
+                {
+                        new ArrayList<Integer>() {{
+                            add(0, 2);
+                            add(1, 1);
+                        }},
+                        new ArrayList<Integer>() {{
+                            add(0, 2);
+                            add(1, 1);
+                        }},
+                        new ArrayList<Integer>() {{
+                            add(0, 4);
+                            add(1, 4);
+                            add(2, 1);
+                        }}
+                },
+                {
+                        new ArrayList<Integer>() {{
+                            add(0, 2);
+                            add(1, 0);
+                            add(2, 0);
+                            add(3, 3);
+                        }},
+                        new ArrayList<Integer>() {{
+                            add(0, 4);
+                            add(1, 0);
+                            add(2, 1);
+                        }},
+                        new ArrayList<Integer>() {{
+                            add(0, 8);
+                            add(1, 0);
+                            add(2, 2);
+                            add(3, 12);
+                            add(4, 0);
+                            add(5, 3);
+                        }}
+                }
+        };
+    }
 }
