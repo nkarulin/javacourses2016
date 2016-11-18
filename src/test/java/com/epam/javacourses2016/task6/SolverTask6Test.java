cb55c7db5921535e7b47031592e07d72fb2d8297
PutienkoEN <justmysimplemail@gmail.com>
2016-11-18 23:00:51 +0300

    Gitignore (not shure it should be here, but i don't know how remove it)
    Task1Test Edit(added \n)
    Task3Test Edit(added foreach + file route + changed data-source + added white space to poem)
    Task4Test Added
    Task6Test Added
    Task7Test Added
    Task19Test Added
    Car class constructor changed from package-privte to public (for test)

--- src/test/java/com/epam/javacourses2016/task6/SolverTask6Test.java
+++ src/test/java/com/epam/javacourses2016/task6/SolverTask6Test.java
@@ -1,13 +1,95 @@
 package com.epam.javacourses2016.task6;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 import static org.testng.Assert.*;
 
 public class SolverTask6Test {
-    @Test
-    public void testAddPolynomials() throws Exception {
-        //TODO
+
+    @Test(dataProvider = "polynomials")
+    public void testAddPolynomials(HashMap<Integer, Integer> first, HashMap<Integer, Integer> second,
+                                   HashMap<Integer, Integer> result) throws Exception {
+
+        SolverTask6 solver = new SolverTask6();
+        Assert.assertEquals(solver.addPolynomials(first,second), result);
     }
 
+    @DataProvider(name = "polynomials")
+    public Object[][] polynomials() {
+        return new Object[][]{
+                {
+                        new HashMap<Integer, Integer>() {{
+                            put(1, 2);
+                            put(2, 3);
+                            put(3, 3);
+                            put(4, 5);
+                        }},
+                        new HashMap<Integer, Integer>() {{
+                            put(1, 4);
+                            put(2, 5);
+                            put(3, 6);
+                            put(4, 1);
+                        }},
+                        new HashMap<Integer, Integer>() {{
+                            put(1, 6);
+                            put(2, 8);
+                            put(3, 9);
+                            put(4, 6);
+                        }}
+                },
+                {
+                        new HashMap<Integer, Integer>() {{
+                            put(1, 2);
+                            put(2, 3);
+                            put(3, 3);
+                            put(4, 5);
+                        }},
+                        new HashMap<Integer, Integer>() {{
+                            put(5, 4);
+                            put(6, 5);
+                            put(7, 6);
+                            put(8, 1);
+                        }},
+                        new HashMap<Integer, Integer>() {{
+                            put(1, 2);
+                            put(2, 3);
+                            put(3, 3);
+                            put(4, 5);
+                            put(5, 4);
+                            put(6, 5);
+                            put(7, 6);
+                            put(8, 1);
+                        }}
+                },
+                {
+
+                        new HashMap<Integer, Integer>() {{
+                            put(1, 4);
+                            put(2, 5);
+                            put(3, 6);
+                            put(4, 1);
+                            put(5, 5);
+                        }},
+                        new HashMap<Integer, Integer>() {{
+                            put(2, 3);
+                            put(3, 3);
+                            put(4, 5);
+                        }},
+                        new HashMap<Integer, Integer>() {{
+                            put(1, 4);
+                            put(2, 8);
+                            put(3, 9);
+                            put(4, 6);
+                            put(5, 5);
+                        }}
+                }
+        };
+    }
 }
