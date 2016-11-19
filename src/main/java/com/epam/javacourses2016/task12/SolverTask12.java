a42a1644a23258877a2b99c8b3140d531c2b4929
PutienkoEN <justmysimplemail@gmail.com>
2016-11-19 20:07:40 +0300

    Task 12 with Test

--- src/main/java/com/epam/javacourses2016/task12/SolverTask12.java
+++ src/main/java/com/epam/javacourses2016/task12/SolverTask12.java
@@ -5,18 +5,38 @@ import java.util.List;
 /**
  * Задан список целых чисел и число X.
  * Не используя вспомогательных объектов и не изменяя размера списка,
- *    переставить элементы списка так, чтобы сначала шли числа, не превосходящие X, а затем числа, большие X.
+ * переставить элементы списка так, чтобы сначала шли числа, не превосходящие X, а затем числа, большие X.
  */
 public class SolverTask12 {
 
     /**
      * Преобразует целочисленный список таким образом, чтобы сначала шли числа меньшие value, затем большие.
+     *
      * @param integers Целочисленный список.
-     * @param value Разделительное значение.
+     * @param value    Разделительное значение.
      * @return Преобразованный список.
      */
     List<Integer> transform(List<Integer> integers, int value) {
-        //TODO
-        return null;
+
+        for (int i = 0; i < integers.size(); i++) {
+            for (int j = 0; j < integers.size() - i - 1; j++) {
+                if (integers.get(j) > integers.get(j + 1)) {
+                    /*
+                    int x = integers.get(j);
+                    int y = integers.get(j + 1);
+                    x = x - y;
+                    y = y + x;
+                    x = y - x;
+                    integers.set(j, x);
+                    integers.set(j + 1, y);
+                    */
+                    integers.set(j, integers.get(j) - integers.get(j + 1));
+                    integers.set(j + 1, integers.get(j + 1) + integers.get(j));
+                    integers.set(j, integers.get(j + 1) - integers.get(j));
+                }
+            }
+        }
+
+        return integers;
     }
 }
--- src/test/java/com/epam/javacourses2016/task12/SolverTask12Test.java
+++ src/test/java/com/epam/javacourses2016/task12/SolverTask12Test.java
@@ -1,13 +1,78 @@
 package com.epam.javacourses2016.task12;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.testng.Assert.*;
 
 public class SolverTask12Test {
-    @Test
-    public void testTransform() throws Exception {
-        //TODO
+    @Test(dataProvider = "nums")
+    public void testTransform(List<Integer> list, int value) throws Exception {
+        SolverTask12 solver = new SolverTask12();
+
+        list = solver.transform(list, value);
+        int elementIndex = list.indexOf(value);
+
+        boolean elementsBeforeSmaller = true;
+        for (int i = 0; i < elementIndex; i++) {
+            if (list.get(i) > value) {
+                elementsBeforeSmaller = false;
+                break;
+            }
+        }
+
+        boolean elemetsAfterGreater = true;
+        for (int i = elementIndex; i < list.size(); i++) {
+            if (list.get(i) < value) {
+                elemetsAfterGreater = false;
+                break;
+            }
+        }
+
+        if (!elemetsAfterGreater || !elementsBeforeSmaller) {
+            String error = "";
+            if (!elemetsAfterGreater) {
+                error += "\nElements after value smaller!";
+            }
+            if (!elementsBeforeSmaller) {
+                error += "\nElements before value greater!";
+            }
+            Assert.fail(error);
+        }
+    }
+
+    @DataProvider(name = "nums")
+    public Object[][] nums() {
+        return new Object[][]{
+                {new ArrayList<Integer>() {{
+                    add(5);
+                    add(4);
+                    add(3);
+                    add(2);
+                    add(1);
+                }}, 5},
+                {new ArrayList<Integer>() {{
+                    add(1);
+                    add(2);
+                    add(3);
+                    add(4);
+                    add(5);
+                }}, 5},
+                {new ArrayList<Integer>() {{
+                    add(6);
+                    add(4);
+                    add(5);
+                    add(8);
+                    add(1);
+                }}, 5},
+                {new ArrayList<Integer>() {{
+                    add(5);
+                }}, 5}
+        };
     }
 
 }
