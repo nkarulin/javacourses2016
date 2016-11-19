ea1043154e99d58a65b9b46222340f31720bd72d
IbrayevRamil <ramil1dan@gmail.com>
2016-11-19 12:01:01 +0300

    task10 done

--- src/main/java/com/epam/javacourses2016/task10/SolverTask10.java
+++ src/main/java/com/epam/javacourses2016/task10/SolverTask10.java
@@ -1,7 +1,11 @@
 package com.epam.javacourses2016.task10;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Задан файл, содержащий английские слова (без знаков препинания).
@@ -17,6 +21,21 @@ public class SolverTask10 {
      * @return Множество пар <слово, количество вхождений в файле>.
      */
     public HashMap<String, Integer> countNumberWords(File input) {
-        return null;
+        List<String> lines = null;
+        try {
+            lines = Files.readAllLines(input.toPath(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        HashMap<String, Integer> words = new HashMap<>();
+        for(String line: lines){
+            if (words.containsKey(line)) {
+                words.put(line, words.get(line) + 1);
+            }
+            else {
+                words.put(line, 1);
+            }
+        }
+        return words;
     }
 }
