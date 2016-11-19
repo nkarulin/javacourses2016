7f79ba8417a187d8b898801fd4cd796769be7e6d
Ivan Fefilov <spb.fefilov.i@gmail.com>
2016-11-19 12:53:09 +0300

    Add solves for 8 and 11 tasks and tests for them;

--- src/test/java/com/epam/javacourses2016/task11/SolverTask11Test.java
+++ src/test/java/com/epam/javacourses2016/task11/SolverTask11Test.java
@@ -1,18 +1,95 @@
 package com.epam.javacourses2016.task11;
 
+import com.epam.javacourses2016.task8.SolverTask8;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 import static org.testng.Assert.*;
 
 public class SolverTask11Test {
-    @Test
-    public void testEmulate() throws Exception {
-        //TODO: with mocked list
+    @Test(enabled = true, dataProvider = "peoples")
+    public void testEmulate(ArrayList<String> list, String result) throws Exception {
+        SolverTask11 task11 = new SolverTask11();
+        Assert.assertEquals(task11.emulate(list), result);
     }
 
-    @Test
-    public void testEmulate1() throws Exception {
-        //TODO: with mocked list
+    @Test(enabled = true, dataProvider = "linked-peoples")
+    public void testEmulate1(LinkedList<String> list, String result) throws Exception {
+        SolverTask11 task11 = new SolverTask11();
+        Assert.assertEquals(task11.emulate(list), result);
+    }
+
+    @DataProvider(name = "peoples")
+    private Object[][] peoples() {
+        return new Object[][] {
+                { new ArrayList<String>(){
+                    {
+                        add("Vanya");
+                        add("Nikita");//
+                        add("Pasha");//
+                        add("Alex");//
+                        add("Maxim");//
+                        add("Anton");//
+                        add("Petya");//
+                        add("Dima");//
+                    }
+                },
+                        "Vanya"
+                },
+                { new ArrayList<String>(){
+                    {
+                        add("Anton");//
+                        add("Petya");//
+                        add("Dima");
+                        add("Vanya");//
+                        add("Nikita");//
+                    }
+
+                },
+                        "Dima"
+                }
+
+
+        };
+    }
+
+    @DataProvider(name = "linked-peoples")
+    private Object[][] linkedPeoples() {
+        return new Object[][] {
+                { new LinkedList<String>(){
+                    {
+                        add("Vanya");
+                        add("Nikita");
+                        add("Pasha");
+                        add("Alex");
+                        add("Maxim");
+                        add("Anton");
+                        add("Petya");
+                        add("Dima");
+                    }
+
+                },
+                        "Vanya"
+                },
+                { new LinkedList<String>(){
+                    {
+                        add("Anton");
+                        add("Petya");
+                        add("Dima");
+                        add("Vanya");
+                        add("Nikita");
+                    }
+
+                },
+                        "Dima"
+                }
+
+
+        };
     }
 
 }
--- src/test/java/com/epam/javacourses2016/task8/SolverTask8Test.java
+++ src/test/java/com/epam/javacourses2016/task8/SolverTask8Test.java
@@ -1,13 +1,26 @@
 package com.epam.javacourses2016.task8;
 
+import com.epam.javacourses2016.task4.SolverTask4;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.HashSet;
 
 import static org.testng.Assert.*;
 
 public class SolverTask8Test {
-    @Test
-    public void testIsNormalBrackets() throws Exception {
-        //TODO
+    @Test(enabled = true, dataProvider = "brackets")
+    public void testIsNormalBrackets(String string, boolean result) throws Exception {
+        SolverTask8 task8 = new SolverTask8();
+        Assert.assertEquals(task8.isNormalBrackets(string), result);
     }
-
+    @DataProvider(name = "brackets")
+    private Object[][] brackets() {
+        return new Object[][]{
+                {"[][{}{",false},
+                {")(x+y)",false},
+                {"(x*2){[x-y]+2}",true}
+        };
+    }
 }
