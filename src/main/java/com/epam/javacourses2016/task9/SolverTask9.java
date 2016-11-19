fe233851bc6aa45227e4df46370e26b117d0bc63
IbrayevRamil <ramil1dan@gmail.com>
2016-11-19 16:02:51 +0300

    task9 done

--- src/main/java/com/epam/javacourses2016/task9/SolverTask9.java
+++ src/main/java/com/epam/javacourses2016/task9/SolverTask9.java
@@ -1,7 +1,11 @@
 package com.epam.javacourses2016.task9;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.HashSet;
+import java.util.List;
 
 /**
  * Задан файл, содержащий английские слова (без знаков препинания).
@@ -16,7 +20,29 @@ public class SolverTask9 {
      * @param input Анализируемый файл.
      * @return Множество полученных слов.
      */
+
     public HashSet<String> getUniqueWords(File input) {
-        return null;
+        List<String> lines = null;
+        try {
+            lines = Files.readAllLines(input.toPath(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        HashSet<String> words = new HashSet<>();
+        for(String line : lines){
+            if (!containsIgnoreCase(words, line)) {
+                words.add(line);
+            }
+        }
+        return words;
+    }
+
+    private static boolean containsIgnoreCase(HashSet<String> words, String word) {
+        for (String s : words) {
+            if (s.equalsIgnoreCase(word)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
