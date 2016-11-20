5187b389706490686562505720d92afc28c662e7
PutienkoEN <justmysimplemail@gmail.com>
2016-11-20 12:48:03 +0300

    Task 16 17 (partly done)

--- src/main/java/com/epam/javacourses2016/task16/SolverTask16.java
+++ src/main/java/com/epam/javacourses2016/task16/SolverTask16.java
@@ -3,7 +3,7 @@ package com.epam.javacourses2016.task16;
 import com.epam.javacourses2016.Point2D;
 
 import java.io.File;
-import java.util.SortedMap;
+import java.util.*;
 
 /**
  * На клетчатой бумаге нарисован круг.
@@ -20,9 +20,80 @@ public class SolverTask16 {
      * @param output Файл для вывода результатов.
      * @return Файл с результатами анализа.
      */
-    IFileWithPoints analyze(Point2D center, int radius, File output) {
-        //TODO
+
+    static IFileWithPoints analyze(Point2D center, int radius) {
+        SortedMap<Integer, Cell> cells = new TreeMap<>();
+        int cellCount = 0;
+
+        for (int indexX = -radius; indexX < radius; indexX++) {
+            for (int indexY = -radius; indexY < radius; indexY++) {
+
+                Point2D point1 = new Point2D(indexX, indexY);
+                Point2D point2 = new Point2D(indexX + 1, indexY);
+                Point2D point3 = new Point2D(indexX, indexY + 1);
+                Point2D point4 = new Point2D(indexX + 1, indexY + 1);
+
+                if (insideCircle(point1, radius, center) && insideCircle(point2, radius, center) &&
+                        insideCircle(point3, radius, center) && insideCircle(point4, radius, center)) {
+
+                    Cell newCell = new Cell(point1, point2, point3, point4);
+                    newCell.countDistance(center);
+                    cells.put(cellCount, newCell);
+                    cellCount++;
+                }
+            }
+        }
+
         return null;
+    }
+
+    static class Cell {
+        Point2D a;
+        Point2D b;
+        Point2D c;
+        Point2D d;
+        double distanceToCenter;
+
+        public Cell(Point2D a, Point2D b, Point2D c, Point2D d) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+        }
+
+        public void countDistance(Point2D point) {
+            double distance1 = distance(point, a);
+            double distance2 = distance(point, b);
+            double distance3 = distance(point, c);
+            double distance4 = distance(point, d);
+
+            distanceToCenter = (distance1 > distance2) ?
+                    distance1 : (distance2 > distance3) ?
+                    distance2 : (distance3 > distance4) ?
+                    distance3 : distance4;
+
+        }
+
+        private double distance(Point2D point1, Point2D point2) {
+            double value = Math.sqrt(Math.pow(point1.getX() - point2.getX(), 2) +
+                    Math.pow(point1.getY() - point2.getY(), 2));
+
+            return value;
+        }
+    }
+
+    public static void main(String[] args) {
+        Point2D point = new Point2D(0, 0);
+        analyze(point, 1);
+    }
+
+    private static boolean insideCircle(Point2D point, int radius, Point2D center) {
+        boolean inside = true;
+        double value = Math.pow(point.getX() - center.getX(), 2) + Math.pow(point.getY() - center.getY(), 2);
+        if (value > Math.pow(radius, 2)) {
+            inside = false;
+        }
+        return inside;
     }
 
     /**
@@ -37,6 +108,7 @@ public class SolverTask16 {
 
         /**
          * Извлекает из файла информацию о хранящихся в нем точках.
+         *
          * @return Множество пар: точка + расстояние до центра.
          */
         SortedMap<Point2D, Double> getPoints();
--- src/main/java/com/epam/javacourses2016/task17/SolverTask17.java
+++ src/main/java/com/epam/javacourses2016/task17/SolverTask17.java
@@ -2,8 +2,11 @@ package com.epam.javacourses2016.task17;
 
 import com.epam.javacourses2016.Point2D;
 import com.epam.javacourses2016.Segment;
+import sun.reflect.generics.tree.Tree;
 
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 /**
  * На плоскости задано N отрезков.
@@ -19,7 +22,48 @@ public class SolverTask17 {
      * @return Множество точек пересечения, имеющих минимальную абсциссу.
      */
     Set<Point2D> analyze(Set<Segment> segments) {
-        //TODO
+        for (int i = 0; i < segments.size(); i++) {
+            for (int j = 0; j < segments.size(); j++) {
+
+            }
+        }
+
         return null;
+    }
+
+    private Point2D getIntersectionPoint(Point2D a, Point2D b, Point2D c, Point2D d) {
+        double x;
+        double y;
+        double x1 = a.getX();
+        double x2 = b.getX();
+        double x3 = c.getX();
+        double x4 = d.getX();
+
+        double y1 = a.getY();
+        double y2 = b.getY();
+        double y3 = c.getY();
+        double y4 = d.getY();
+        double k1 = (x2 - x1) / (y2 - y1);
+        double k2 = (x4 - x3) / (y4 - y3);
+
+        if (k1 == k2) {
+            return null;
+        }
+
+        x = ((x1 * y2 - x2 * y1) * (x4 - x3) - (x3 * y4 - x4 * y3) * (x2 - x1)) /
+                ((y1 - y2) * (x4 - x3) - (y3 - y4) * (x2 - x1));
+        y = ((y3 - y4) * x - (x3 * y4 - x4 * y3)) / (x4 - x3);
+
+        if (onLine(x1, x2, x3, x4, x) || onLine(y1, y2, y3, y4, y)) {
+            return null;
+        }
+        return new Point2D(x, y);
+    }
+
+    private boolean onLine(double x1, double x2, double x3, double x4, double x) {
+        if ((x1 <= x) && (x2 >= x) && (x3 <= x) && (x4 >= x)) {
+            return true;
+        }
+        return false;
     }
 }
