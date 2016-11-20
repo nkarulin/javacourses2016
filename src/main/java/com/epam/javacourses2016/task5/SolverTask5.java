c094d8b4dfc88fd5b849869a047cce0bf9be625d
PutienkoEN <justmysimplemail@gmail.com>
2016-11-20 23:34:57 +0300

    Task 5, Partly Test 5

--- src/main/java/com/epam/javacourses2016/task5/SolverTask5.java
+++ src/main/java/com/epam/javacourses2016/task5/SolverTask5.java
@@ -1,5 +1,7 @@
 package com.epam.javacourses2016.task5;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 /**
@@ -10,7 +12,6 @@ import java.util.List;
  * Для повышения точности вычислений использовать класс {@link java.math.BigDecimal}
  */
 public class SolverTask5 {
-
     /**
      * Вычисляет сопротивление методом наименьших квадратов.
      *
@@ -18,7 +19,43 @@ public class SolverTask5 {
      * @return Вычисленное по исходным данным сопротивление.
      */
     double calcResistance(List<Measurement> measurements) {
-        //TODO
-        return 0;
+        double xSum = 0;
+        double ySum = 0;
+        double xSqrSum = 0;
+        double xySum = 0;
+
+        for (Measurement measure : measurements) {
+            xSum += measure.getCurrent();
+            ySum += measure.getVoltage();
+            xSqrSum += Math.pow(measure.getCurrent(), 2);
+            xySum += measure.getCurrent() * measure.getVoltage();
+        }
+
+        double measureCount = measurements.size();
+
+        double a;
+        double b;
+        a = ((measureCount * xySum) - (xSum * ySum)) / (measureCount * xSqrSum - xSum * xSum);
+        b = (ySum - a * xSum) / measureCount;
+
+        BigDecimal aBig;
+        BigDecimal bBig;
+        aBig = new BigDecimal((measureCount * xySum - (xSum * ySum)) / (measureCount * xSqrSum - xSum * xSum));
+        bBig = BigDecimal.valueOf(ySum).subtract(aBig.multiply(BigDecimal.valueOf(xSum)))
+                .divide(BigDecimal.valueOf(measureCount), RoundingMode.CEILING);
+
+        double e = 0;
+        BigDecimal eBig = BigDecimal.ZERO;
+        for (Measurement measure : measurements) {
+
+            BigDecimal funcBig = BigDecimal.valueOf(measure.getVoltage())
+                    .subtract(aBig.multiply(BigDecimal.valueOf(measure.getCurrent())).add(bBig));
+            eBig = eBig.add(funcBig.pow(2));
+
+            double func = measure.getVoltage() - (a * measure.getCurrent() + b);
+            e += Math.pow(func, 2);
+        }
+
+        return eBig.doubleValue();
     }
 }
--- src/test/java/com/epam/javacourses2016/task5/SolverTask5Test.java
+++ src/test/java/com/epam/javacourses2016/task5/SolverTask5Test.java
@@ -1,11 +1,33 @@
 package com.epam.javacourses2016.task5;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SolverTask5Test {
-    @Test
-    public void testCalcResistance() throws Exception {
-        //TODO:
+
+    @Test(enabled = true, dataProvider = "measurements")
+    public void testCalcResistance(double[][] measurements) throws Exception {
+        SolverTask5 solver = new SolverTask5();
+        List<Measurement> listMeasures = getMeasurements(measurements);
+        solver.calcResistance(listMeasures);
     }
 
+    public List<Measurement> getMeasurements(double[][] measurements) {
+        List<Measurement> list = new ArrayList<>();
+
+        for (double[] measure : measurements) {
+            list.add(new Measurement(measure[0], measure[1]));
+        }
+        return list;
+    }
+
+    @DataProvider(name = "measurements")
+    public Object[][] measurements() {
+        return new Object[][]{
+                {new double[][]{{1, 5.3}, {2, 6.3}, {3, 4.8}, {4, 3.8}, {5, 3.3}}}
+        };
+    }
 }
