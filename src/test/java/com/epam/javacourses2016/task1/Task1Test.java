9216858603bd95e2ef439999932a7a4ca1a875d5
giuliorm <giulio.rm95@gmail.com>
2016-11-23 05:37:00 +0300

    Added \r\n symbols after every string while writing strings to the file. Null checking for every string added. Test testEmptyLinesList added.

--- src/test/java/com/epam/javacourses2016/task1/Task1Test.java
+++ src/test/java/com/epam/javacourses2016/task1/Task1Test.java
@@ -8,27 +8,66 @@ import java.io.*;
 import java.lang.reflect.Array;
 import java.net.URL;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class Task1Test {
+
+    /**
+    Creates an example of a file for usage in tests.
+
+    @param lines Lines to be placed to the file. May be null or empty,
+                 in this case an empty string will be placed to the file.
+
+
+    @param prefix A string, used in generation of a file name. May be {@code null }
+    @param suffix A string, used in generation of a file name. May be {@code null },
+                  in this case default {@code *.tmp} will be used.
+    @return A File object created or throws an exception if any failures occured.
+    @throws IOException
+                 This exception will be checked further in tests.
+
+    **/
+    private File createFile(List<String> lines, String prefix, String suffix) throws IOException{
+        File in = Files.createTempFile(prefix, suffix != null ? suffix : "tmp").toFile();
+        String empty = "";
+        if (lines != null && lines.size() > 0)
+            try (FileWriter write = new FileWriter(in)) {
+                for (String line : lines) {
+                        write.write(line != null ? line  : empty);
+                        write.write("\r\n");
+                }
+            }
+
+        return in;
+    }
+
+    @Test(enabled = false)
+    public void testEmptyLinesList() throws IOException {
+        List<String> lines = new ArrayList<>();
+        SolverTask1 solver = new SolverTask1();
+
+        File in = createFile(lines, "task1", "in");
+
+        File out = Files.createTempFile("task1", "out").toFile();
+
+        Assert.assertEquals(solver.reverseFile(in, out), lines);
+
+        Collections.reverse(lines);
+        try (BufferedReader reader = new BufferedReader(new FileReader(out))) {
+            for (String line : lines) {
+                Assert.assertEquals(reader.readLine(), line);
+            }
+        }
+    }
 
     @Test(enabled = false, dataProvider = "lines")
     public void testTask1(String[] linesArray) throws IOException {
         List<String> lines = Arrays.asList(linesArray);
         SolverTask1 solver = new SolverTask1();
 
-        File in = Files.createTempFile("task1", "in").toFile();
-
-        try (FileWriter write = new FileWriter(in)) {
-            for (String line : lines) {
-                write.write(line);
-                write.write("\r\n");
-            }
-        }
+        File in = createFile(lines, "task1", "in");
 
         File out = Files.createTempFile("task1", "out").toFile();
 
