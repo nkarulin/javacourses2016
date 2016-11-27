8b1f6e0ce6247d5aab3d07f2cc08808f068b2928
giuliorm <giulio.rm95@gmail.com>
2016-11-27 05:36:33 +0300

    tasks 6 and 7 solved

--- src/main/java/com/epam/javacourses2016/task6/SolverTask6.java
+++ src/main/java/com/epam/javacourses2016/task6/SolverTask6.java
@@ -1,6 +1,8 @@
 package com.epam.javacourses2016.task6;
 
 import java.util.HashMap;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Сложить два многочлена, если коэффициенты многочленов хранятся в объекте HashMap.
@@ -16,6 +18,22 @@ public class SolverTask6 {
      * @return Многочлен, полученный в результате сложения.
      */
     public HashMap<Integer, Integer> addPolynomials(HashMap<Integer, Integer> first, HashMap<Integer, Integer> second) {
-        return null;
+        HashMap<Integer, Integer> polySum = new HashMap<>();
+        Set<Integer> keys = first.keySet().stream().collect(Collectors.toSet());
+        keys.addAll(second.keySet());
+        for(Integer key : keys) {
+
+            boolean firstContains = first.containsKey(key);
+            boolean secondContains =  second.containsKey(key);
+
+            if (firstContains && secondContains) {
+                polySum.put(key, first.get(key) + second.get(key));
+            } else if (firstContains && !secondContains) {
+                polySum.put(key, first.get(key));
+            } else if (secondContains && !firstContains) {
+                polySum.put(key, second.get(key));
+            }
+        }
+        return polySum;
     }
 }
