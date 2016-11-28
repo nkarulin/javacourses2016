e5a861e52c39add9f94d06880ab989eecef1c9b3
Tatyana Voroshilova <tanyabellka@gmail.com>
2016-11-28 12:21:40 +0300

    task3 done

--- src/main/java/com/epam/javacourses2016/task3/SolverTask3.java
+++ src/main/java/com/epam/javacourses2016/task3/SolverTask3.java
@@ -1,7 +1,9 @@
 package com.epam.javacourses2016.task3;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 /**
  * Занести строки, составляющие стихотворения указанного автора, в список.
@@ -11,12 +13,26 @@ public class SolverTask3 {
 
     /**
      * Формирует упорядоченный список строк из стихотворений указанного автора.
-     * @param poems Анализируемое множество стихотворений.
+     *
+     * @param poems  Анализируемое множество стихотворений.
      * @param author Автор, стихотворения которого необходимо выбрать.
      * @return Список, упорядоченных по длине строк, составляющих стихотворения автора.
      */
     public List<String> sortPoems(Set<Poem> poems, String author) {
-        //TODO: realize solver of a task
-        return null;
+        List<String> authorLines = new ArrayList<>();
+        poems.stream()
+                .filter(poem -> poem.getAuthor().equals(author))
+                .map(Poem::getLines)
+                .forEach(authorLines::addAll);
+
+//        for (Poem p : poems) {
+//            if (Objects.equals(p.getAuthor(), author)) {
+//                authorLines.addAll(p.getLines());
+//            }
+//        }
+        authorLines.sort((a1,a2)->a1.compareTo(a2));
+
+
+        return authorLines;
     }
 }
