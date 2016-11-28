0fbec6eb8b08f94973d342c54f8b752d48e51d07
Tatyana Voroshilova <tanyabellka@gmail.com>
2016-11-28 11:26:05 +0300

    task1 done

--- src/main/java/com/epam/javacourses2016/task1/SolverTask1.java
+++ src/main/java/com/epam/javacourses2016/task1/SolverTask1.java
@@ -1,7 +1,13 @@
 package com.epam.javacourses2016.task1;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Ввести строки из файла, записать в список.
@@ -17,6 +23,31 @@ public class SolverTask1 {
      */
     public List<String> reverseFile(File input, File output) {
         //TODO: realize solver of a task
-        return null;
+        Scanner s = null;
+        ArrayList<String> list = new ArrayList<String>();
+        try {
+            s = new Scanner(input);
+
+            while (s.hasNextLine()){
+                list.add(s.nextLine());
+            }
+            s.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        List<String> reverseList = list.subList(0, list.size());
+        Collections.reverse(reverseList);
+
+        try {
+            FileWriter writer = new FileWriter(output);
+            for (String str:reverseList){
+                writer.append(str+"\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
