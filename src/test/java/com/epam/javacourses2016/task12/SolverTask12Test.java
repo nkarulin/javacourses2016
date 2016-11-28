9ad5a257000538cc76e622dbda1271a00fe5593a
PutienkoEN <justmysimplemail@gmail.com>
2016-11-28 23:14:01 +0300

    Test 12, 19 edits. Test 12 edited sign. Test 19 edited data provider.

--- src/test/java/com/epam/javacourses2016/task12/SolverTask12Test.java
+++ src/test/java/com/epam/javacourses2016/task12/SolverTask12Test.java
@@ -20,10 +20,10 @@ public class SolverTask12Test {
         boolean previousGreater = false;
 
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) >= value) {
+            if (list.get(i) > value) {
                 previousGreater = true;
             }
-            if (list.get(i) < value && previousGreater) {
+            if (list.get(i) <= value && previousGreater) {
                 wrongSort = true;
                 break;
             }
