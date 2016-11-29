bff54eb36054fb582f545ff4e927141a570c5d4c
IbrayevRamil <ramil1dan@gmail.com>
2016-11-29 14:24:19 +0300

    из-за смены кодировки, не проходит проверку, возвращаю назад на UTF-8....)

--- src/test/java/com/epam/javacourses2016/task3/Task3Test.java
+++ src/test/java/com/epam/javacourses2016/task3/Task3Test.java
@@ -18,9 +18,9 @@ public class Task3Test {
     @DataProvider(name = "poems")
     private Object[][] poems() {
         return new Object[][]{
-                {"poem1_sorted.txt", "Пушкин", new String[]{"poem1.txt", "poem2.txt"}},
-                {"poem2_sorted.txt", "Крылов", new String[]{"poem3.txt", "poem2.txt"}},
-                {"poem3_sorted.txt", "Бродский", new String[]{"poem1.txt", "poem3.txt"}},
+                {"poem1_sorted.txt", "РџСѓС€РєРёРЅ", new String[]{"poem1.txt", "poem2.txt"}},
+                {"poem2_sorted.txt", "РљСЂС‹Р»РѕРІ", new String[]{"poem3.txt", "poem2.txt"}},
+                {"poem3_sorted.txt", "Р‘СЂРѕРґСЃРєРёР№", new String[]{"poem1.txt", "poem3.txt"}},
         };
     }
 
