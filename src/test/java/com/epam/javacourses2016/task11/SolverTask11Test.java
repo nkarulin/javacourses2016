86012cc720c699a7d5b693e10bd6b98bcce220f2
giuliorm <giulio.rm95@gmail.com>
2016-11-30 00:48:07 +0400

    task 11 test?

--- src/test/java/com/epam/javacourses2016/task11/SolverTask11Test.java
+++ src/test/java/com/epam/javacourses2016/task11/SolverTask11Test.java
@@ -1,11 +1,16 @@
 package com.epam.javacourses2016.task11;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 import static org.testng.Assert.*;
 
 public class SolverTask11Test {
-    @Test
+    @Test()
     public void testEmulate() throws Exception {
         //TODO: with mocked list
     }
@@ -13,6 +18,66 @@ public class SolverTask11Test {
     @Test
     public void testEmulate1() throws Exception {
         //TODO: with mocked list
+    }
+
+    @DataProvider(name = "arraylists")
+    public Object[][] arrayLists() {
+        return new Object[][]{
+                {
+                    new ArrayList<String>() {{
+                        this.add("First");
+                        this.add("Second");
+                        this.add("Third");
+                        this.add("Fourth");
+                    }},
+                    "Fourth"
+                },
+                {
+                        new ArrayList<String>() {{
+                            this.add("First");
+                            this.add("Second");
+                            this.add("Third");
+                        }},
+                        "Third"
+                },
+                {
+                        new ArrayList<String>() {{
+                            this.add("First");
+                        }},
+                        "First"
+                },
+
+        };
+    }
+
+    @DataProvider(name = "linkedlists")
+    public Object[][] linkedLists() {
+        return new Object[][]{
+                {
+                        new LinkedList() {{
+                            this.add("First");
+                            this.add("Second");
+                            this.add("Third");
+                            this.add("Fourth");
+                        }},
+                        "Fourth"
+                },
+                {
+                        new LinkedList() {{
+                            this.add("First");
+                            this.add("Second");
+                            this.add("Third");
+                        }},
+                        "Third"
+                },
+                {
+                        new LinkedList() {{
+                            this.add("First");
+                        }},
+                        "First"
+                },
+
+        };
     }
 
 }
