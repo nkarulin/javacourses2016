b1b74809cd78536a3a815e399a4b1bf341beca9d
IbrayevRamil <ramil1dan@gmail.com>
2016-11-30 17:49:47 +0300

    task14 done

---
+++ src/main/java/com/epam/javacourses2016/task14/MyNumberCollection.java
@@ -0,0 +1,97 @@
+package com.epam.javacourses2016.task14;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by Рамиль on 30.11.2016.
+ */
+public class MyNumberCollection<T extends Number> implements AbstractCollectionCreator.NumberCollection<T> {
+
+    private List<T> numberList;
+
+    public MyNumberCollection() {
+        numberList = new ArrayList<>();
+    }
+
+    @Override
+    public Number nearest(Number value) {
+        double minDif = 0;
+        T nearestValue = null;
+        for (T curNum : numberList) {
+            if (Math.abs(value.doubleValue() - curNum.doubleValue()) <= minDif) {
+                nearestValue = curNum;
+            }
+        }
+        return nearestValue;
+    }
+
+    @Override
+    public int size() {
+        return numberList.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return numberList.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return numberList.contains(o);
+    }
+
+    @Override
+    public Iterator iterator() {
+        return numberList.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return numberList.toArray();
+    }
+
+    @Override
+    public boolean add(T o) {
+        return numberList.add(o);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return numberList.remove(o);
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return numberList.addAll(c);
+    }
+
+    @Override
+    public void clear() {
+        numberList.clear();
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return numberList.retainAll(c);
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+
+        return numberList.removeAll(c);
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+
+        return numberList.containsAll(c);
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return numberList.toArray(a);
+    }
+}
