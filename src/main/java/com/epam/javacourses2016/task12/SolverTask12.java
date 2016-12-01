a524d6be7742cf94d149588adc151f47948dc145
Yuhans <yushin.tema@gmail.com>
2016-12-01 20:52:21 +0300

    change task_12

--- src/main/java/com/epam/javacourses2016/task12/SolverTask12.java
+++ src/main/java/com/epam/javacourses2016/task12/SolverTask12.java
@@ -1,6 +1,6 @@
 package com.epam.javacourses2016.task12;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,22 +17,32 @@ public class SolverTask12 {
      * @param value    Разделительное значение.
      * @return Преобразованный список.
      */
-    List<Integer> transform(List<Integer> integers, int value) {
-        int smallerCount = 0;
-        for (Integer integer : integers) {
-            if (integer < value) {
-                smallerCount++;
-            }
-        }
-        for (int i = 0; i < integers.size(); i++) {
-            if (integers.get(i) >= value) {
-                for (int j = smallerCount; j < integers.size(); j++) {
-                    if (integers.get(j) < value) {
-                        Collections.swap(integers, i, j);
-                    }
-                }
-            }
-        }
+    static List<Integer> transform(List<Integer> integers, int value) {
+//        int smallerCount = 0;
+//        for (Integer integer : integers) {
+//            if (integer < value) {
+//                smallerCount++;
+//            }
+//        }
+//        for (int i = 0; i < integers.size(); i++) {
+//            if (integers.get(i) >= value) {
+//                for (int j = smallerCount; j < integers.size(); j++) {
+//                    if (integers.get(j) < value) {
+//                        Collections.swap(integers, i, j);
+//                    }
+//                }
+//            }
+//        }
+        integers.sort((integer, o2) -> integer.compareTo(value));
         return integers;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 5; i >= 0; i--) {
+            integers.add(i*i);
+        }
+        System.out.println(integers);
+        System.out.println(transform(integers, 25));
     }
 }
