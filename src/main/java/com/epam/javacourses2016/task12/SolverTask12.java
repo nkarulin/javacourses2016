497dd250242e60bd645b149f92ae9ee8bf019c48
Yuhans <yushin.tema@gmail.com>
2016-12-01 21:00:46 +0300

    task_12 done

--- src/main/java/com/epam/javacourses2016/task12/SolverTask12.java
+++ src/main/java/com/epam/javacourses2016/task12/SolverTask12.java
@@ -1,6 +1,5 @@
 package com.epam.javacourses2016.task12;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,32 +16,8 @@ public class SolverTask12 {
      * @param value    Разделительное значение.
      * @return Преобразованный список.
      */
-    static List<Integer> transform(List<Integer> integers, int value) {
-//        int smallerCount = 0;
-//        for (Integer integer : integers) {
-//            if (integer < value) {
-//                smallerCount++;
-//            }
-//        }
-//        for (int i = 0; i < integers.size(); i++) {
-//            if (integers.get(i) >= value) {
-//                for (int j = smallerCount; j < integers.size(); j++) {
-//                    if (integers.get(j) < value) {
-//                        Collections.swap(integers, i, j);
-//                    }
-//                }
-//            }
-//        }
+    List<Integer> transform(List<Integer> integers, int value) {
         integers.sort((integer, o2) -> integer.compareTo(value));
         return integers;
-    }
-
-    public static void main(String[] args) {
-        List<Integer> integers = new ArrayList<>();
-        for (int i = 5; i >= 0; i--) {
-            integers.add(i*i);
-        }
-        System.out.println(integers);
-        System.out.println(transform(integers, 25));
     }
 }
