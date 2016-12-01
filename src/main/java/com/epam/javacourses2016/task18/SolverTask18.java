b5f83c2e055eb6eb7d0edaf3303539e514e0c99b
PutienkoEN <justmysimplemail@gmail.com>
2016-12-01 06:41:08 +0300

    Task 18 + Test 18

--- src/main/java/com/epam/javacourses2016/task18/SolverTask18.java
+++ src/main/java/com/epam/javacourses2016/task18/SolverTask18.java
@@ -1,5 +1,8 @@
 package com.epam.javacourses2016.task18;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Дана матрица из целых чисел.
  * Найти в ней прямоугольную подматрицу, состоящую из максимального количества одинаковых элементов.
@@ -7,13 +10,127 @@ package com.epam.javacourses2016.task18;
  */
 public class SolverTask18 {
     /**
-     * @param matrix Анализируемая матрица.
+     * @param myArrayMatrix Анализируемая матрица.
      * @return Подматрица, состоящая из максимального количества одинаковых элементов.
      */
-    RectangularIntegerMatrix getMaxSubMatrix(RectangularIntegerMatrix matrix) {
+    private int myArrayMatrix[][];
+    private int rowCount = 1;
+    private int colCount = 1;
+    private int startRow = 0;
+    private int startCol = 0;
+    List<RectangularIntegerMatrix> listOfMatrix = new ArrayList<>();
 
+    RectangularIntegerMatrix getMaxSubMatrix(RectangularIntegerMatrix matrix) {
+        myArrayMatrix = toArrayMatrix(matrix);
+        for (int i = 0; i < myArrayMatrix.length; i++) {
+            for (int j = 0; j < myArrayMatrix[i].length - 1; j++) {
+                if (myArrayMatrix[i][j] == myArrayMatrix[i][j + 1]) {
+                    rowCount++;
+                    startRow = i;
+                    startCol = j;
+                    checkCloseCells(i, j, myArrayMatrix[i][j]);
+                }
+            }
+        }
 
         return null;
+    }
+
+    private void checkCloseCells(int row, int col, int value) {
+        boolean haveBotRow = true;
+        boolean haveRightCol = true;
+
+        haveBotRow = checkBotRow(row, col, value);
+        haveRightCol = checkRightCol(row, col, value);
+
+        if (haveBotRow && haveRightCol) {
+            checkCloseCells(row + 1, col + 1, value);
+        }
+
+        if (!haveBotRow && haveRightCol) {
+            while (haveRightCol) {
+                haveRightCol = checkRightCol(row, col, value);
+            }
+        }
+
+        if (!haveRightCol && haveBotRow) {
+            while (haveBotRow) {
+                haveBotRow = checkBotRow(row, col, value);
+            }
+        }
+
+        MyMatrix myMatrix = new MyMatrix(startRow, startCol, rowCount, colCount, value);
+        listOfMatrix.add(myMatrix);
+    }
+
+    private boolean checkBotRow(int row, int col, int value) {
+        if (row + 1 >= myArrayMatrix.length - 1) {
+            return false;
+        }
+        
+        boolean haveBotRow = true;
+        for (int i = 0; i < colCount; i++) {
+            if (myArrayMatrix[row + 1][col + i] != value) {
+                haveBotRow = false;
+            }
+        }
+
+        rowCount++;
+        return haveBotRow;
+    }
+
+    private boolean checkRightCol(int row, int col, int value) {
+        if (col + 1 > myArrayMatrix[0].length - 1) {
+            return false;
+        }
+
+        boolean haveRightCol = true;
+        for (int i = 0; i < rowCount; i++) {
+            if (myArrayMatrix[row + i][col + 1] != value) {
+                haveRightCol = false;
+            }
+        }
+
+        colCount++;
+        return haveRightCol;
+    }
+
+    private int[][] toArrayMatrix(RectangularIntegerMatrix matrix) {
+        int[][] arrayMatrix = new int[matrix.getHeight()][matrix.getWidth()];
+
+        for (int i = 0; i < arrayMatrix.length; i++) {
+            for (int j = 0; j < arrayMatrix[0].length; j++) {
+                arrayMatrix[i][j] = matrix.getValue(i, j);
+            }
+        }
+
+        return arrayMatrix;
+    }
+
+    class MyMatrix implements RectangularIntegerMatrix {
+
+        public MyMatrix() {
+
+        }
+
+        public MyMatrix(int startX, int startY, int row, int col, int value) {
+
+        }
+
+        @Override
+        public int getWidth() {
+            return 0;
+        }
+
+        @Override
+        public int getHeight() {
+            return 0;
+        }
+
+        @Override
+        public int getValue(int indexWidth, int indexHeight) {
+            return 0;
+        }
     }
 
     /**
--- src/test/java/com/epam/javacourses2016/task18/SolverTask18Test.java
+++ src/test/java/com/epam/javacourses2016/task18/SolverTask18Test.java
@@ -1,5 +1,7 @@
 package com.epam.javacourses2016.task18;
 
+import com.epam.javacourses2016.task17.SolverTask17;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
@@ -7,9 +9,39 @@ import org.testng.annotations.Test;
  */
 public class SolverTask18Test {
 
-    @Test
-    public void testGetMaxSubMatrix() throws Exception {
-        //TODO
+    @Test(enabled = true, dataProvider = "matrixData")
+    public void testGetMaxSubMatrix(int[][] matrixData) throws Exception {
+        SolverTask18 solver = new SolverTask18();
+        TestMatrix testMatrix = new TestMatrix(matrixData);
+        solver.getMaxSubMatrix(testMatrix);
     }
 
+    @DataProvider(name = "matrixData")
+    public Object[][] matrixData () {
+        return new Object[][] {
+                {new int[][]{{1,2,3},{2,1,1},{3,1,1}}}
+        };
+    }
+
+    class TestMatrix implements SolverTask18.RectangularIntegerMatrix {
+        int[][] matrix;
+
+        public TestMatrix(int[][] matrix) {
+            this.matrix = matrix;
+        }
+        @Override
+        public int getWidth() {
+            return matrix.length;
+        }
+
+        @Override
+        public int getHeight() {
+            return matrix[0].length;
+        }
+
+        @Override
+        public int getValue(int indexWidth, int indexHeight) {
+            return matrix[indexWidth][indexHeight];
+        }
+    }
 }
