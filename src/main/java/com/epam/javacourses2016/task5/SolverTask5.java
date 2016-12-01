9afc0647d22198ea9cb5a7a1075db072ee1a0e69
IbrayevRamil <ramil1dan@gmail.com>
2016-12-01 18:16:06 +0300

    task 5 fixed, test5 enabled

--- src/main/java/com/epam/javacourses2016/task5/SolverTask5.java
+++ src/main/java/com/epam/javacourses2016/task5/SolverTask5.java
@@ -1,6 +1,7 @@
 package com.epam.javacourses2016.task5;
 
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -20,42 +21,16 @@ public class SolverTask5 {
      * @param measurements Измерения в ходе эксперимента.
      * @return Вычисленное по исходным данным сопротивление.
      */
-    double calcResistance(List<Measurement> measurements) {
-        Double sumXi = 0.0;
-        Double sumYi= 0.0;
-        Double sumXiYi= 0.0;
-        Double sumSqrXi= 0.0;
-        for (Measurement measurement : measurements) {
-            sumXi += measurement.getCurrent();
-            sumYi += measurement.getVoltage();
-            sumXiYi += measurement.getCurrent()*measurement.getVoltage();
-            sumSqrXi += measurement.getCurrent()*measurement.getCurrent();
-        }
-        int n = measurements.size();
-        Double x = 0.0;
-        Double y = 0.0;
-        if ((sumSqrXi*n)-(sumXi*sumXi) != 0) {
-            x = ((sumXiYi*n)-(sumXi*sumYi))/((sumSqrXi*n)-(sumXi*sumXi));
-            y = ((sumSqrXi*sumYi)-(sumXiYi*sumXi))/((sumSqrXi*n)-(sumXi*sumXi));
-        }
-        Double e = 0.0;
-        for (Measurement measurement : measurements) {
-            e += Math.pow(((measurement.getVoltage())-(optimalFuncValue(measurement.getCurrent(),x,y))),2);
-        }
-        return round(e, 3);
+    static double calcResistance(List<Measurement> measurements) {
+            BigDecimal Syx = new BigDecimal(0);
+            for (Measurement measurement : measurements) {
+                Syx = Syx.add(new BigDecimal(measurement.getCurrent()*measurement.getVoltage()));
+            }
+            BigDecimal Sxx = new BigDecimal(0);
+            for (Measurement measurement : measurements) {
+                Sxx = Sxx.add(new BigDecimal(measurement.getCurrent()*measurement.getCurrent()));
+            }
+            BigDecimal R = Syx.divide(Sxx,6);
+            return R.doubleValue();
     }
-
-    private static double round(double value, int places) {
-        if (places < 0) throw new IllegalArgumentException();
-
-        long factor = (long) Math.pow(10, places);
-        value = value * factor;
-        long tmp = Math.round(value);
-        return (double) tmp / factor;
-    }
-
-    private static Double optimalFuncValue(Double x, Double coef1, Double coef2) {
-        return coef1*x+coef2;
-    }
-
 }
