83484bfc06a4a32adf0327c5e19f4abea0f59533
Tatyana Voroshilova <tanyabellka@gmail.com>
2016-12-01 21:53:53 +0300

    50% Task10 (not tested)

--- src/main/java/com/epam/javacourses2016/task10/SolverTask10.java
+++ src/main/java/com/epam/javacourses2016/task10/SolverTask10.java
@@ -1,7 +1,11 @@
 package com.epam.javacourses2016.task10;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * Задан файл, содержащий английские слова (без знаков препинания).
@@ -13,10 +17,29 @@ public class SolverTask10 {
 
     /**
      * Подсчитывает количество вхождений каждого слова в файле.
+     *
      * @param input Файл с исходными данными.
      * @return Множество пар <слово, количество вхождений в файле>.
      */
     public HashMap<String, Integer> countNumberWords(File input) {
-        return null;
+        HashMap<String, Integer> result = new HashMap<>();
+        String str;
+        try (FileReader fr = new FileReader(input); BufferedReader br = new BufferedReader(fr)) {
+            while ((str = br.readLine()) != null) {
+                String[] words = str.split("\\|");
+                for (String s : words) {
+                    if (result.containsKey(s)) {
+                        result.put(s, result.get(s) + 1);
+                    } else {
+                        result.put(s, 1);
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+
     }
 }
