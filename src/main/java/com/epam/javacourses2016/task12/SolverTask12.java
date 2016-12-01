4f50c6a47273196451c39502560125a518d070d6
Yuhans <yushin.tema@gmail.com>
2016-12-01 20:38:44 +0300

    change task_12

--- src/main/java/com/epam/javacourses2016/task12/SolverTask12.java
+++ src/main/java/com/epam/javacourses2016/task12/SolverTask12.java
@@ -1,26 +1,38 @@
 package com.epam.javacourses2016.task12;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Задан список целых чисел и число X.
  * Не используя вспомогательных объектов и не изменяя размера списка,
- *    переставить элементы списка так, чтобы сначала шли числа, не превосходящие X, а затем числа, большие X.
+ * переставить элементы списка так, чтобы сначала шли числа, не превосходящие X, а затем числа, большие X.
  */
 public class SolverTask12 {
 
     /**
      * Преобразует целочисленный список таким образом, чтобы сначала шли числа меньшие value, затем большие.
+     *
      * @param integers Целочисленный список.
-     * @param value Разделительное значение.
+     * @param value    Разделительное значение.
      * @return Преобразованный список.
      */
     List<Integer> transform(List<Integer> integers, int value) {
-        integers.sort((o1, o2) -> {
-            if (o1 > value) return 1;
-            if (o1 < value) return -1;
-            return 0;
-        });
+        int smallerCount = 0;
+        for (Integer integer : integers) {
+            if (integer < value) {
+                smallerCount++;
+            }
+        }
+        for (int i = 0; i < integers.size(); i++) {
+            if (integers.get(i) >= value) {
+                for (int j = smallerCount; j < integers.size(); j++) {
+                    if (integers.get(j) < value) {
+                        Collections.swap(integers, i, j);
+                    }
+                }
+            }
+        }
         return integers;
     }
 }
