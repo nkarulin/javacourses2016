1a8738bc92a005e5af9bd3819713721cd598e4f4
Tatyana Voroshilova <tanyabellka@gmail.com>
2016-12-01 19:16:42 +0300

    50% Task5

--- src/main/java/com/epam/javacourses2016/task5/SolverTask5.java
+++ src/main/java/com/epam/javacourses2016/task5/SolverTask5.java
@@ -1,10 +1,11 @@
 package com.epam.javacourses2016.task5;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
  * Интерфейс для юнит-тестирования задания №5.
- *
+ * <p>
  * Список содержит результаты измерений тока и напряжения на неизвестном сопротивлении R.
  * Найти приближенное число R методом наименьших квадратов.
  * Для повышения точности вычислений использовать класс {@link java.math.BigDecimal}
@@ -18,7 +19,18 @@ public class SolverTask5 {
      * @return Вычисленное по исходным данным сопротивление.
      */
     double calcResistance(List<Measurement> measurements) {
-        //TODO
-        return 0;
+        BigDecimal R;
+        double sumI = 0;
+        double sumU = 0;
+        double sumUI = 0;
+        double sum_sqrtI = 0;
+        for (Measurement m : measurements) {
+            sumI += m.getCurrent();
+            sumU += m.getVoltage();
+            sumUI += m.getCurrent() * m.getVoltage();
+            sum_sqrtI += Math.pow(m.getCurrent(), 2);
+        }
+        R = new BigDecimal((measurements.size() * sumUI - sumI * sumU) / (measurements.size() * sum_sqrtI - Math.pow(sumI, 2)));
+        return R.setScale(3, BigDecimal.ROUND_DOWN).doubleValue();
     }
 }
