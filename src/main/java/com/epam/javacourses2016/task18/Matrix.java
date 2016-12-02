15fa3cdbe2ebab9dcf0418679b7af82daa538a6c
Ivan Fefilov <spb.fefilov.i@gmail.com>
2016-12-02 17:16:53 +0300

    Added equals and hashcode for Task18 in source class(Matrix.class)

--- src/main/java/com/epam/javacourses2016/task18/Matrix.java
+++ src/main/java/com/epam/javacourses2016/task18/Matrix.java
@@ -1,5 +1,7 @@
 package com.epam.javacourses2016.task18;
 
+import java.util.Arrays;
+
 /**
  * Created by Ivan on 01/12/2016.
  */
@@ -28,5 +30,21 @@ public class Matrix implements SolverTask18.RectangularIntegerMatrix {
     @Override
     public int getValue(int indexWidth, int indexHeight) {
         return matrix[indexWidth][indexHeight];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Matrix matrix1 = (Matrix) o;
+
+        return Arrays.deepEquals(matrix, matrix1.matrix);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(matrix);
     }
 }
