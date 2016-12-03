097cd51f668119d4d7cde5d5fd3ba46f8044e5d7
anton <anton.kiselev.94@inbox.ru>
2016-12-03 14:59:05 +0300

    without test

--- src/main/java/com/epam/javacourses2016/task1/SolverTask1.java
+++ src/main/java/com/epam/javacourses2016/task1/SolverTask1.java
@@ -1,6 +1,8 @@
 package com.epam.javacourses2016.task1;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,8 +17,20 @@ public class SolverTask1 {
      * @param output Файл с выходными данными.
      * @return Список строк, прочитанных из входного файла в прямом порядке.
      */
-    public List<String> reverseFile(File input, File output) {
-        //TODO: realize solver of a task
-        return null;
+    public List<String> reverseFile(File input, File output) throws IOException {
+        List<String> stringList = new ArrayList<>();
+        String s;
+        try(BufferedReader in = new BufferedReader(new FileReader(input))) {
+            while((s = in.readLine()) != null) {
+                stringList.add(s);
+            }
+        }
+        try( BufferedWriter out = new BufferedWriter(new FileWriter(output)) ) {
+            for(int i = stringList.size()-1; i >= 0; i--) {
+                out.write(stringList.get(i));
+                out.write("\n");
+            }
+        }
+        return stringList;
     }
 }
--- src/main/java/com/epam/javacourses2016/task2/SolverTask2.java
+++ src/main/java/com/epam/javacourses2016/task2/SolverTask2.java
@@ -1,6 +1,9 @@
 package com.epam.javacourses2016.task2;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -14,7 +17,22 @@ public class SolverTask2 {
      * @return Множество элементов корневого каталога и подкаталогов.
      */
     public Set<File> getFiles(File directory) {
-        //TODO: realize solver of a task
-        return null;
+        Set<File> fileSet = new HashSet<>();
+        getFilesInDirectory(directory, fileSet);
+        System.out.println(fileSet);
+        return fileSet;
+    }
+
+    private void getFilesInDirectory(File directory, Set<File> fileSet) {
+        if(directory.exists()) {
+            fileSet.add(directory);
+            if(directory.isDirectory() && (directory.list() != null) && (directory.listFiles() != null)) {
+                List<File> files = Arrays.asList(directory.listFiles());
+                for(File file : files) {
+                    fileSet.add(file);
+                    getFilesInDirectory(file,fileSet);
+                }
+            }
+        }
     }
 }
