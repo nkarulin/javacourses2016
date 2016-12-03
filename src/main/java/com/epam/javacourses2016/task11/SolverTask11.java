9f7adc2e71b46819a8fc35260727f432e8956428
PutienkoEN <justmysimplemail@gmail.com>
2016-12-03 06:09:46 +0300

    Task 11 with Test

--- src/main/java/com/epam/javacourses2016/task11/SolverTask11.java
+++ src/main/java/com/epam/javacourses2016/task11/SolverTask11.java
@@ -1,8 +1,8 @@
 package com.epam.javacourses2016.task11;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * В кругу стоят N человек.
@@ -20,8 +20,12 @@ public class SolverTask11 {
      */
     public String emulate(ArrayList<String> peoples) {
 
+        if (peoples == null || peoples.size() == 0) {
+            throw new NullPointerException();
+        }
+
         int index = 0;
-        while (peoples.size() > 1) {
+        while (peoples.size() != 1) {
             index += 1;
             if (index >= peoples.size()) {
                 index = index % 2;
@@ -39,23 +43,39 @@ public class SolverTask11 {
      * @return Имя последнего оставшегося.
      */
     public String emulate(LinkedList<String> peoples) {
+
+        /*
+        Bad way - this will be n^2, because peoples.get(i) will iterate through all peoples(before i)
+        to find i element, that's why i using iterator here.
+        for (int i = 0; i < peoples.size(); i++) {
+            peoples.get(i);
+        }
+        */
+
+        if (peoples == null || peoples.size() == 0) {
+            throw new NullPointerException();
+        }
+
         int index = 0;
+        ListIterator iterator = peoples.listIterator();
+        iterator.next();
 
-        while (peoples.size() > 1) {
-            Iterator iterator = peoples.iterator();
+        while (peoples.size() != 1) {
             iterator.next();
-            if (index == 1) {
-                iterator.next();
-            }
-            index += 1;
+            index++;
 
+            //If we out of range - recreate iterator and move it for zero[0] o first[1] element
             if (index >= peoples.size()) {
                 index = index % 2;
+                iterator = peoples.listIterator();
+                iterator.next();
+                if (index == 1) {
+                    iterator.next();
+                }
             }
 
             iterator.remove();
         }
         return peoples.get(0);
-
     }
 }
--- src/test/java/com/epam/javacourses2016/task11/SolverTask11Test.java
+++ src/test/java/com/epam/javacourses2016/task11/SolverTask11Test.java
@@ -9,7 +9,7 @@ import java.util.LinkedList;
 
 public class SolverTask11Test {
 
-    @Test(enabled = false, dataProvider = "peoples")
+    @Test(enabled = true, dataProvider = "peoples")
     public void testEmulate(String[] peoples, String survived) throws Exception {
         ArrayList<String> array = new ArrayList<>();
 
@@ -21,7 +21,7 @@ public class SolverTask11Test {
         Assert.assertEquals(solver.emulate(array), survived);
     }
 
-    @Test(enabled = false, dataProvider = "peoples")
+    @Test(enabled = true, dataProvider = "peoples")
     public void testEmulate1(String[] peoples, String survived) throws Exception {
         LinkedList<String> array = new LinkedList<>();
 
@@ -36,6 +36,7 @@ public class SolverTask11Test {
     @DataProvider(name = "peoples")
     public Object[][] peoples() {
         return new Object[][]{
+                {new String[]{"Vasya"}, "Vasya"},
                 {new String[]{"Vasya", "Petya", "Dima"}, "Dima"},
                 {new String[]{"Vasya", "Petya", "Dima", "Alex"}, "Vasya"}
         };
