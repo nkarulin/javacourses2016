691db8250e840ac56588bbcf58e106e474a4a5d3
Yuhans <yushin.tema@gmail.com>
2016-12-05 21:36:49 +0300

    first try task_19

--- src/main/java/com/epam/javacourses2016/task19/SolverTask19.java
+++ src/main/java/com/epam/javacourses2016/task19/SolverTask19.java
@@ -2,7 +2,7 @@ package com.epam.javacourses2016.task19;
 
 import com.epam.javacourses2016.Car;
 
-import java.util.Set;
+import java.util.*;
 
 /**
  * На кольцевой гоночной трассе стоит N автомобилей.
@@ -19,6 +19,35 @@ public class SolverTask19 {
      */
     int getNumberOvertaking(Set<Car> cars, long lengthLap, int numberLaps) {
         //TODO
-        return 0;
+        long length = numberLaps * lengthLap;
+        ArrayList<Car> sortedByPosition = new ArrayList<>(cars);
+        int farAhead = 0;
+        sortedByPosition.sort(Comparator.comparingInt(Car::getCurrentPosition));
+        for (Car car : sortedByPosition) {
+            car.setFarAhead(farAhead);
+            farAhead++;
+        }
+
+        boolean endOfRace = false;
+        while (!endOfRace) {
+            endOfRace = true;
+            for (Car car : sortedByPosition) {
+                if (car.getCurrentPosition() + car.getSpeed() <= length) {
+                    car.setCurrentPosition(car.getCurrentPosition() + car.getSpeed());
+                    endOfRace = false;
+                } else
+                    car.setCurrentPosition((int)length);
+            }
+            sortedByPosition.sort(Comparator.comparingInt(Car::getCurrentPosition));
+        }
+        int count = 0;
+        farAhead = 0;
+        for (Car car : sortedByPosition) {
+            if (farAhead > car.getFarAhead()) {
+                count = count + (farAhead - car.getFarAhead());
+            }
+            farAhead++;
+        }
+        return count;
     }
 }
