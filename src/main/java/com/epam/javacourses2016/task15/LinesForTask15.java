466ef2145efa6846845f4a416e20e0324f85d4ad
Tatyana Voroshilova <tanyabellka@gmail.com>
2016-12-06 22:14:45 +0300

    task15 50%

---
+++ src/main/java/com/epam/javacourses2016/task15/LinesForTask15.java
@@ -0,0 +1,61 @@
+package com.epam.javacourses2016.task15;
+
+
+public class LinesForTask15 {
+    double k;
+    double b;
+    int number;
+    int id;
+
+    public LinesForTask15(double k, double b, int number, int id) {
+        this.k = k;
+        this.b = b;
+        this.number = number;
+        this.id = id;
+    }
+
+    public double getK() {
+        return k;
+    }
+
+    public void setK(double k) {
+        this.k = k;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+
+
+    public void addNumber() {
+        this.number++;
+    }
+
+
+
+
+}
+
--- src/main/java/com/epam/javacourses2016/task15/SolverTask15.java
+++ src/main/java/com/epam/javacourses2016/task15/SolverTask15.java
@@ -3,7 +3,8 @@ package com.epam.javacourses2016.task15;
 import com.epam.javacourses2016.Point2D;
 
 import java.io.File;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * На плоскости задано N точек.
@@ -12,13 +13,51 @@ import java.util.Set;
 public class SolverTask15 {
     /**
      * Осуществляет анализ переданных точек, вычисляя линии, которые проходят более чем через 2 точки.
+     *
      * @param points Множество точек на плоскости.
      * @param output Файл для вывода результатов.
      * @return Файл с результатами анализа.
      */
+
+
     IFileWithLines analyze(Set<Point2D> points, File output) {
         //TODO
+        List<LinesForTask15> list = new ArrayList<>();
+        double x1, x2, y1, y2, k, b;
+        for (Point2D p : points) {
+            x1 = p.getX();
+            y1 = p.getY();
+            for (Point2D p2 : points) {
+                x2 = p2.getX();
+                y2 = p2.getY();
+                k = (y2 - y1) / (x2 - x1);
+                b = (x2 * y1 - x1 * y2) / (x2 - x1);
+                if (!existInList(k, b, list)) {
+                    addLine(k, b, list);
+                }
+            }
+        }
+        Iterator<LinesForTask15> iterator = list.iterator();
+        LinesForTask15 l
+        while (iterator.hasNext()){
+            l=iterator.next();
+            if (l.getNumber()<3){
+                iterator.remove();
+            }
+        }
         return null;
+    }
+
+    private boolean existInList(double k, double b, List<LinesForTask15> list) {
+        return list.stream()
+                .filter((item -> item.k == k) & (item -> item.b == b))
+                .forEach(item -> item.addNumber())
+                .anyMatch;
+
+    }
+
+    private void addLine(double k, double b, List<LinesForTask15> list) {
+        list.add(new LinesForTask15(k, b, 1, list.size()));
     }
 
     /**
@@ -33,6 +72,7 @@ public class SolverTask15 {
 
         /**
          * Извлекает из файла информацию о хранящихся в нем линиях.
+         *
          * @return Множество линий, найденных в результате анализа.
          */
         Set<ILine> getLines();
@@ -43,7 +83,9 @@ public class SolverTask15 {
      */
     interface ILine {
 
-        /** @return Точки, через которые проходит прямая */
+        /**
+         * @return Точки, через которые проходит прямая
+         */
         Set<Point2D> getPoints();
     }
 }
