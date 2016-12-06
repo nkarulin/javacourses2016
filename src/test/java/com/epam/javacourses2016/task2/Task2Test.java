bef6550f2e226c015d74b50e0fb940e75cbb6089
Yuhans <yushin.tema@gmail.com>
2016-12-06 11:41:42 +0300

    add one part of test_2

--- src/test/java/com/epam/javacourses2016/task2/Task2Test.java
+++ src/test/java/com/epam/javacourses2016/task2/Task2Test.java
@@ -16,7 +16,7 @@ import java.util.Set;
 public class Task2Test {
 
 
-    @Test(enabled = false, dataProvider = "directory")
+    @Test(enabled = true, dataProvider = "directory")
     public void testTask2(String directory) { //общее навзвание, другие люди свои тесты тодже сюда вписывают,  зза этого будет конфликт на мастере
         SolverTask2 solver = new SolverTask2();
         File file = new File(directory);
