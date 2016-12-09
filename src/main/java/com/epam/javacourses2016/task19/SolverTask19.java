3266b3d45aabf7ecc8179327ad7f9788330de901
eseev-german <eseev.german@yandex.ru>
2016-12-09 15:43:52 +0300

    Task19

--- src/main/java/com/epam/javacourses2016/task19/SolverTask19.java
+++ src/main/java/com/epam/javacourses2016/task19/SolverTask19.java
@@ -2,6 +2,7 @@ package com.epam.javacourses2016.task19;
 
 import com.epam.javacourses2016.Car;
 
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -18,7 +19,25 @@ public class SolverTask19 {
      * @return Количество осуществленных обгонов.
      */
     int getNumberOvertaking(Set<Car> cars, long lengthLap, int numberLaps) {
-        //TODO
-        return 0;
+        int result = 0;
+        while (!cars.isEmpty()) {
+
+            Iterator<Car> iterator = cars.iterator();
+            Car car1 = iterator.next();
+            iterator.remove();
+            while (iterator.hasNext()) {
+                Car car2 = iterator.next();
+                double time1 = lengthLap * numberLaps / car1.getSpeed();
+                double time2 = lengthLap * numberLaps / car2.getSpeed();
+                if (time1 < time2) {
+                    int leftLap = (int) (numberLaps - (double) car2.getSpeed() * time1 / lengthLap);
+                    result += car1.getStartPosition() < car2.getStartPosition() ? leftLap + 1 : leftLap;
+                } else {
+                    int leftLap = (int) (numberLaps - (double) car1.getSpeed() * time2 / lengthLap);
+                    result += car2.getStartPosition() < car1.getStartPosition() ? leftLap + 1 : leftLap;
+                }
+            }
+        }
+        return result;
     }
 }
