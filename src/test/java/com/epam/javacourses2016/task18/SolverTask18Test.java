b83f81ff03076b000b4d54ef56d09ea56a998d76
IbrayevRamil <ramil1dan@gmail.com>
2016-12-16 00:09:53 +0300

    task18 done

--- src/test/java/com/epam/javacourses2016/task18/SolverTask18Test.java
+++ src/test/java/com/epam/javacourses2016/task18/SolverTask18Test.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
  */
 public class SolverTask18Test {
 
-    @Test(enabled = false, dataProvider = "matrix")
+    @Test(enabled = true, dataProvider = "matrix")
     public void testGetMaxSubMatrix(int[][] data, int[][] result) throws Exception {
         SolverTask18 task18 = new SolverTask18();
         SolverTask18.RectangularIntegerMatrix matrix = task18.getMaxSubMatrix(convertToMatrix(data));
@@ -20,10 +20,18 @@ public class SolverTask18Test {
     @DataProvider(name = "matrix")
     public Object[][] matrix() {
         return new Object[][]{
-                {new int[][]{{1, 1, 1, 2, 2}, {1, 2, 1, 3, 4}, {1, 6, 4, 2, 5}}, new int[][]{{1, 1, 1}, {1, 2, 1}, {1, 6, 4}}},
-                {new int[][]{{1, 2, 1, 1}, {3, 4, 1, 1}, {2, 5, 3, 2}}, new int[][]{{1, 2, 1, 1}, {3, 4, 1, 1}}}
+                {new int[][]{{5, 5, 8},
+                             {5, 5, 7},
+                             {1, 3, 4}},
+                        new int[][]{{5, 5},
+                                    {5, 5}}},
+                {new int[][]{{6, 6, 6, 1},
+                             {6, 6, 6, 1},
+                             {2, 2, 2, 2}}, new int[][]{{6, 6, 6}, {6, 6, 6}}}
         };
     }
+
+
 
     private SolverTask18.RectangularIntegerMatrix convertToMatrix(int[][] arr) {
         return new Matrix(arr);
