a062394d559ce584ea75344a843e51f9eb136677
Tatyana Voroshilova <tanyabellka@gmail.com>
2016-12-16 14:06:38 +0300

    add deleted test11

--- src/test/java/com/epam/javacourses2016/task11/SolverTask11Test.java
+++ src/test/java/com/epam/javacourses2016/task11/SolverTask11Test.java
@@ -17,5 +17,33 @@ import java.util.List;
 import static org.testng.Assert.*;
 
 public class SolverTask11Test {
+    @Test(enabled = true, dataProvider = "people")
+    public void testEmulate(String[] arr, String result) throws Exception {
+        SolverTask11 task11 = new SolverTask11();
+        Assert.assertEquals(task11.emulate(convertToArrayList(arr)), result);
+    }
 
+    @Test(enabled = false, dataProvider = "people")
+    public void testEmulate1(String[] arr, String result) throws Exception {
+        SolverTask11 task11 = new SolverTask11();
+        Assert.assertEquals(task11.emulate(convertToLinkedList(arr)), result);
+    }
+
+    public ArrayList<String> convertToArrayList(String[] arr) {
+
+        return new ArrayList<String>(Arrays.asList(arr));
+    }
+
+    public LinkedList<String> convertToLinkedList(String[] arr) {
+
+        return new LinkedList<String>(Arrays.asList(arr));
+    }
+
+    @DataProvider(name = "people")
+    private Object[][] peoples() {
+        return new Object[][]{
+                {new String[]{"Vanya", "Nikita", "Pasha", "Alex", "Maxim", "Anton", "Petya", "Dima"}, "Dima"},
+                {new String[]{"Anton", "Petya", "Dima", "Vanya", "Nikita"}, "Petya"}
+        };
+    }
 }
