5397243d7812ac618d76e3fdd9ce2ef3fa788c2e
Tatyana Voroshilova <tanyabellka@gmail.com>
2016-12-16 09:13:03 +0300

    80% task19

--- src/main/java/com/epam/javacourses2016/task19/SolverTask19.java
+++ src/main/java/com/epam/javacourses2016/task19/SolverTask19.java
@@ -2,10 +2,7 @@ package com.epam.javacourses2016.task19;
 
 import com.epam.javacourses2016.Car;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * На кольцевой гоночной трассе стоит N автомобилей.
@@ -22,25 +19,42 @@ public class SolverTask19 {
      */
     int getNumberOvertaking(Set<Car> cars, long lengthLap, int numberLaps) {
         //TODO
-        HashMap<Integer,Integer> hashMap=new HashMap<>();
+//        HashMap<Integer,Integer> hashMap=new HashMap<>();
         List<Integer> arr = new ArrayList<>();
-        for (Car car : cars) {
-            car.setDistance(car.getSpeed()*numberLaps+car.getStartPosition());
-            car.setLaps((int) (car.getDistance()/lengthLap));
-            arr.add(car.getLaps());
+
+        List<Car> carsList = new LinkedList<>();
+        carsList.addAll(cars);
+
+        Collections.sort(carsList, (a, b) -> {
+            Integer speedA=a.getSpeed();
+            Integer speedB=b.getSpeed();
+            return speedA.compareTo(speedB);
+        });
+
+        Collections.reverse(carsList);
+        int maxSpeed=carsList.get(0).getSpeed();
+        int time= (int) (lengthLap*numberLaps/maxSpeed);
+
+        System.out.println("LengthLap="+lengthLap+" numberLaps"+numberLaps);
+        System.out.println("maxSpeed="+maxSpeed+"time="+time);
+        for (Car car : carsList) {
+            car.setDistance(car.getSpeed()*time);
+            arr.add(car.getDistance());
         }
 
+        System.out.println("arr of distance:"+arr.toString());
         int overlaps=0;
         if ((arr.get(0)==0)||(arr.get(1)==0)){
             overlaps=0;
             return overlaps;
         }
         if (arr.get(0)>arr.get(1)){
-            overlaps=(int)arr.get(0)/arr.get(1);
+            overlaps= (int) ((arr.get(0)-arr.get(1)) /lengthLap);
         }
-        else{
-            overlaps=(int)arr.get(1)/arr.get(0);
-        }
+
+//        else{
+//            overlaps=(int)arr.get(1)/arr.get(0);
+//        }
 
         return overlaps;
     }
