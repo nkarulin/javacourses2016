55c05e081ff053f718e9ebc10c1c538e54d16994
KeiZaal <gafarov.dilshod@gmail.com>
2016-12-19 18:00:37 +0300

    task 17 done

--- src/main/java/com/epam/javacourses2016/task17/SolverTask17.java
+++ src/main/java/com/epam/javacourses2016/task17/SolverTask17.java
@@ -3,10 +3,7 @@ package com.epam.javacourses2016.task17;
 import com.epam.javacourses2016.Point2D;
 import com.epam.javacourses2016.Segment;
 
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * На плоскости задано N отрезков.
@@ -22,36 +19,45 @@ public class SolverTask17 {
      * @return Множество точек пересечения, имеющих минимальную абсциссу.
      */
     public Set<Point2D> analyze(Set<Segment> segments) {
-        TreeMap <Point2D, Double> points = new TreeMap<>(new MyComparator());
+        TreeMap <Double, HashSet<Point2D>> points = new TreeMap<>(new MyComparator());
         Set <Point2D> output = new HashSet<>();
         segments.stream().forEach((s1) -> {
             Line line1 = new Line(s1.getA(), s1.getB());
             segments.stream().forEach((s2) -> {
                 Line line2 = new Line(s2.getA(), s2.getB());
                 if (line1.getA() != line2.getA()) {
-                    Point2D point = new Point2D((line2.getB() - line1.getB())/(line1.getA() - line2.getA()), (line2.getB() * line1.getA() - line2.getA() * line1.getB())/(line1.getA() - line2.getA()));
+                    Point2D point = new Point2D((line2.getB() - line1.getB()) / (line1.getA() - line2.getA()), (line2.getB() * line1.getA() - line2.getA() * line1.getB()) / (line1.getA() - line2.getA()));
                     double maxX = Math.max(s1.getA().getX(), Math.max(s1.getB().getX(), Math.max(s2.getA().getX(), s2.getB().getX())));
                     double minX = Math.min(s1.getA().getX(), Math.min(s1.getB().getX(), Math.min(s2.getA().getX(), s2.getB().getX())));
                     double maxY = Math.max(s1.getA().getY(), Math.max(s1.getB().getY(), Math.max(s2.getA().getY(), s2.getB().getY())));
                     double minY = Math.min(s1.getA().getY(), Math.min(s1.getB().getY(), Math.min(s2.getA().getY(), s2.getB().getY())));
+
                     if (point.getX() >= minX && point.getX() <= maxX && point.getY() >= minY && point.getY() <= maxY) {
-                        points.put(point, point.getX());
+                        if (points.containsKey(point.getX())) {
+                            points.get(point.getX()).add(point);
+                        } else {
+                            HashSet<Point2D> pointsSet = new HashSet<>();
+                            pointsSet.add(point);
+                            points.put(point.getX(), pointsSet);
+                        }
                     }
                 }
             });
         });
-        output.add(points.firstKey());
-        points.keySet().stream().forEach((k) -> {
-            if (k.getX() == points.firstKey().getX()) output.add(k);
-        });
-        return output;
+
+        if (!points.isEmpty())
+            return points.firstEntry().getValue();
+        else return new HashSet<Point2D>();
     }
 
-    private class MyComparator implements Comparator<Point2D> {
+    private class MyComparator implements Comparator<Double> {
 
         @Override
-        public int compare(Point2D p1, Point2D p2) {
-            return (int) (p1.getX() - p2.getX());
+        public int compare(Double x1, Double x2) {
+            if (x1 > x2) return 1;
+            else if (x1 < x2) return -1;
+            else return 0;
+
         }
 
         @Override
